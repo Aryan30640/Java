ae4377953f39b6344f9e474c37f751fa0649e1d3
Satyam <aryan30640@gmail.com>
2024-11-05 06:59:02 +0530

    Loops

---
+++ NumLoops.java
@@ -0,0 +1,26 @@
+public class ZeroToTen {
+    public static void main(String[] args) {
+        //counter++ ==> counter + 1
+        //counter is a changable variable
+
+
+        //for(int counter = 0; counter < 11; counter++) {
+        //    System.out.println(counter);
+        //}
+        
+
+        //int i = 0;
+        //while(i < 11) {
+        //    System.out.println(i);
+        //    i = i + 1; // i++;
+        //}
+
+
+        int i = 0;
+        do { 
+            System.out.println(i);
+            i = i + 1; // i++;
+        } while ( i < 11);
+    }
+}
+
--- ZeroToTen.java
+++
@@ -1,9 +0,0 @@
-public class ZeroToTen {
-    public static void main(String[] args) {
-        //counter++ ==> counter + 1
-        //counter is a changable variable
-        for(int counter = 0; counter < 11; counter++) {
-            System.out.println(counter);
-        }
-    }
-}
