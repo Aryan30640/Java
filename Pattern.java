46409d00427a02950ce04643bb1b1d8908b4bac2
Satyam <aryan30640@gmail.com>
2024-11-06 09:08:37 +0530

    updated

--- Pattern.java
+++ Pattern.java
@@ -4,7 +4,9 @@ public class Pattern {
         int m = 4;
         int n = 5;
 
+        //Outer loop
         for(int i = 1; i <= m; i = i + 1) {
+            //Inner loop
             for(int j = 1; j <= n; j = j + 1) {
                 System.out.print("*");
             }
