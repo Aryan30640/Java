b31e519135960166a79a3e6dc6f1cc64beb6dc82
Satyam <aryan30640@gmail.com>
2024-11-07 13:20:51 +0530

    Updated

--- Practice/SolRhom.java
+++ Practice/SolRhom.java
@@ -2,7 +2,6 @@ public class SolRhom {
     public static void main(String[] args) {
         int n = 5;
 
-
         for(int i = 1; i <= n; i++) {
             //spaces
             for (int j = 1; j <= n - i; j++) {
