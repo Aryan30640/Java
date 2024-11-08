f6dd66a56a64efce18ea24c6ca7d983ef6dc58e5
Satyam <aryan30640@gmail.com>
2024-11-08 13:18:34 +0530

    Updated

---
+++ Shapes/InvTraig01.java
@@ -0,0 +1,17 @@
+public class InvTraig01 {
+    public static void main(String[] args) {
+            int n = 4;
+            for(int i=n; i>=1; i--) {
+            for(int j=1; j<=i; j++) {
+                int sum = i+j;
+                if(sum%2==0){
+                    System.out.print("0 ");
+                }
+                else {
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
