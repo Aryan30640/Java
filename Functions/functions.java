9844d1bca807bdd361cdbb643bfdd7fb3b3629ac
Satyam <aryan30640@gmail.com>
2024-11-11 15:18:44 +0530

    Updated

--- Functions/functions.java
+++ Functions/functions.java
@@ -1,7 +1,15 @@
+import  java.util.*;
 
 public class functions {
-
+    public static void printMyName(String name) {
+        System.out.println(name);
+        System.out.println("exiting with code 0");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        try (Scanner sc = new Scanner(System.in)) {
+            String name = sc.next();
+
+            printMyName(name);
+        }
     }
 }
