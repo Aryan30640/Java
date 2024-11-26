bd985310ccd80247008e22c89926c8a6db817fba
aryan30640 <aryan30640@gmail.com>
2024-11-26 13:10:24 +0530

    Updated

--- Exercise/Avg3.java
+++ Exercise/Avg3.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 public class Avg3 {
 
     public static void main(String[] args) {
-        System.out.println("Enter Three Numbers To Get Their Average.");
+        System.out.print("Enter Three Numbers To Get Their Average: ");
         try (Scanner sc = new Scanner(System.in)) {
             int a = sc.nextInt();
-                System.out.println("Next Number:");
+                System.out.print("Next Number:");
             int b = sc.nextInt();
-            System.out.println("Next Number:");
+            System.out.print("Next Number:");
             int c = sc.nextInt();
             int sum = (a + b + c);
             int avg = (sum/3);
             System.out.println(avg);
         }
-
     }
 }
